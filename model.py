f436499ee6d6fc98d26d36b34ed7797ddea7a058
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-03 13:04:19 +0300

    model_save_load_implemented

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-
-from consts import IMG_W,IMG_H,N_CHANNELS
+import numpy as np
+from consts import IMG_W,IMG_H,N_CHANNELS, BATCH_SIZE
 
 class Model():
     def __init__(self):
@@ -44,9 +44,41 @@ class Model():
         for m in range(len(self._models)):
             self._models[m].fit_generator(gen.flow(train_images,train_labels[:,m]),validation_data=gen.flow(val_images,val_labels[:,m]),steps_per_epoch=len(train_images)/(batch_size),epochs=epochs)
 
+    def _get_model_filename(self,path_to_file,index):
+        return path_to_file+'_{}'.format(index)
+
     def save(self,path_to_file):
         for m in range(len(self._models)):
-            self._models[m].save_weights(path_to_file+'_{}'.format(m),save_format='tf')
+            self._models[m].save_weights(self._get_model_filename(path_to_file, m),save_format='tf')
+
+    def load(self,path_fo_file,classes_list):
+
+        self._models=[]
+        for index in range(len(classes_list)):
+            model_file_path=self._get_model_filename(path_fo_file,index)
+            model=self._get_single_model(classes_list[index])
+            model.load_weights(model_file_path)
+            self._models.append(model)
+
+    def predict(self, gen, images):
+
+        pred_labels=[]
+        flow=gen.flow(images,shuffle=False, batch_size=BATCH_SIZE)
+        max_batch_idx=len(flow)
+        for batch_idx, img_batch in enumerate(flow):
+            predictions_batch=[m.predict(img_batch) for m in self._models]
+            labels_batch=[np.argmax(p,axis=-1) for p in predictions_batch]
+            labels=np.stack(labels_batch,axis=-1)
+            pred_labels.append(labels)
+
+            if batch_idx==max_batch_idx-1:
+                break
+
+        pred_labels=np.concatenate(pred_labels,axis=0)
+        return pred_labels
+
+
+
 
 
 
--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from model import Model
 from image_data_generator import ImageDataGenerator
 from consts import DATA_DIR,MODEL_NAME,BATCH_SIZE,EPOCHS,LR, TRAIN_DATASET_PKL, VAL_DATASET_PKL, MODELS_DIR
 
+debug_regime=False
 
 if __name__ == "__main__":
 
@@ -19,6 +20,13 @@ if __name__ == "__main__":
 
     with open(os.path.join(DATA_DIR,VAL_DATASET_PKL),'rb') as file:
         val_images, val_labels, _ = pickle.load(file)
+
+    if debug_regime:
+        max_samples=100
+        train_images=train_images[:max_samples]
+        train_labels=train_labels[:max_samples]
+        val_images=val_images[:max_samples]
+        val_labels=val_labels[:max_samples]
 
     print('{} train images loaded'.format(len(train_images)))
     print('{} val images loaded'.format(len(val_images)))
@@ -31,5 +39,18 @@ if __name__ == "__main__":
     model.compile(classes_list=classes, optimizer=keras.optimizers.Adam(lr=LR),loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit_generator(gen,train_images,train_labels, val_images,val_labels,batch_size=BATCH_SIZE,epochs=EPOCHS)
 
-    model.save(os.path.join(DATA_DIR,MODELS_DIR,MODEL_NAME))
+    model_filepath=os.path.join(DATA_DIR,MODELS_DIR,MODEL_NAME)
+    model.save(model_filepath)
+
+    model_loaded=Model()
+    model_loaded.load(model_filepath, classes)
+
+    val_preds=model_loaded.predict(gen, val_images)
+    acc=[float(all(val_pred==val_label)) for val_pred,val_label in zip(val_preds,val_labels)]
+    print('validation accuracy of loaded model = {}'.format(np.mean(acc)))
+
+
+
+
+
 
