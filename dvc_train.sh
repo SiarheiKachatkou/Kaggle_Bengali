9f29db29c4dd602085f84f551a6a162c0f86fcda
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-04 17:24:40 +0300

    pytorch_model_no_augmentation

--- dvc_train.sh
+++ dvc_train.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+git_cmt_message=$1
+
+set -x
+
 PY=/home/sergey/anaconda3/envs/tensorflow/bin/python
 
-deps_code="-d consts.py -d train.py -d model.py -d image_data_generator.py"
+deps_code=""
+for p in *.py
+do
+deps_code=$deps_code -d $p
+done
+
+
 deps_data="-d data/test_datset.pkl -d data/train_datset.pkl -d data/val_datset.pkl -d data/image_gen.pkl"
 
 outputs="data/models"
 metric="metric.txt"
-dvc run $deps -f $outputs.dvc -o $outputs -M $metric $PY train.py
+dvc run $deps_code #deps_data -f $outputs.dvc -o $outputs -M $metric $PY train.py
 dvc push $outputs.dvc
+
+git commit -a -m $git_cmt_message
