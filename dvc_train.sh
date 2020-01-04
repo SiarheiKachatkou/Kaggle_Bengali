8af4d4e95bddcf1f00bd23e93b41d7f2154e30b8
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-04 17:25:12 +0300

    pytorch_model_no_augmentation

--- dvc_train.sh
+++ dvc_train.sh
@@ -17,7 +17,7 @@ deps_data="-d data/test_datset.pkl -d data/train_datset.pkl -d data/val_datset.p
 
 outputs="data/models"
 metric="metric.txt"
-dvc run $deps_code #deps_data -f $outputs.dvc -o $outputs -M $metric $PY train.py
+dvc run $deps_code $deps_data -f $outputs.dvc -o $outputs -M $metric $PY train.py
 dvc push $outputs.dvc
 
 git commit -a -m $git_cmt_message
