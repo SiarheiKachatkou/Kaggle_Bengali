7fb686b2295298129fedc43bd9d60b9b735392e4
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-04 19:54:20 +0300

    dataset_with_croped_symbols_128_128

--- create_dataset.py
+++ create_dataset.py
@@ -62,7 +62,7 @@ if __name__=="__main__":
     classes = [sum(class_map['component_type']==target) for target in TARGETS]
 
     imgs,image_ids,labels = load_parquet(os.path.join(DATA_DIR,RAW_DIR, TRAIN_IMAGE_DATA_PATTERN),train_csv)
-    imgs=[crop_symbol(img) for img in imgs]
+    imgs=np.array([crop_symbol(img) for img in imgs])
 
     skf = sklearn.model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=SEED)
     #TODO place for improvements
@@ -78,7 +78,7 @@ if __name__=="__main__":
     dump(os.path.join(DATA_DIR,VAL_DATASET_PKL), imgs_val,labels_val,ids_val, classes)
 
     test_imgs,test_image_ids,test_labels = load_parquet(os.path.join(DATA_DIR, RAW_DIR, TEST_IMAGE_DATA_PATTERN),labels_csv=None)
-    test_imgs=[crop_symbol(img) for img in test_imgs]
+    test_imgs=np.array([crop_symbol(img) for img in test_imgs])
     dump(os.path.join(DATA_DIR,TEST_DATASET_PKL), test_imgs,test_labels,test_image_ids, classes)
 
 
