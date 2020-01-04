83acd0f38928dd9ec50e5b76b9e20ba3f51f3195
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-04 19:43:54 +0300

    dataset_with_croped_symbols_128_128

--- create_dataset.py
+++ create_dataset.py
@@ -8,7 +8,7 @@ from create_dataset_utils import dump,load
 
 from consts import DATA_DIR,RAW_DIR,TRAIN_IMAGE_DATA_PATTERN, TEST_IMAGE_DATA_PATTERN, IMG_HEIGHT,IMG_WIDTH,N_CHANNELS,TRAIN_CSV,CLASS_MAP_CSV, IMG_H,IMG_W, TRAIN_DATASET_PKL, VAL_DATASET_PKL, TEST_DATASET_PKL, IMAGE_GEN_PKL, SEED, TARGETS
 
-debug_mode=False
+debug_mode=True
 
 def preproc(x):
     x=cv2.resize(np.reshape(x,[IMG_HEIGHT,IMG_WIDTH,N_CHANNELS]),(IMG_W,IMG_H))
@@ -42,7 +42,16 @@ def load_parquet(path_pattern, labels_csv):
     image_ids=np.array(image_ids)
     return imgs,image_ids,labels
 
+def crop_symbol(img):
 
+    black=np.where(img<230)
+    left,right = min(black[1]), max(black[1])
+    top,bottom = min(black[0]), max(black[0])
+    img=img[top:bottom,left:right]
+
+    img=preproc(img)
+
+    return img
 
 if __name__=="__main__":
 
@@ -53,6 +62,7 @@ if __name__=="__main__":
     classes = [sum(class_map['component_type']==target) for target in TARGETS]
 
     imgs,image_ids,labels = load_parquet(os.path.join(DATA_DIR,RAW_DIR, TRAIN_IMAGE_DATA_PATTERN),train_csv)
+    imgs=[crop_symbol(img) for img in imgs]
 
     skf = sklearn.model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=SEED)
     #TODO place for improvements
@@ -68,6 +78,7 @@ if __name__=="__main__":
     dump(os.path.join(DATA_DIR,VAL_DATASET_PKL), imgs_val,labels_val,ids_val, classes)
 
     test_imgs,test_image_ids,test_labels = load_parquet(os.path.join(DATA_DIR, RAW_DIR, TEST_IMAGE_DATA_PATTERN),labels_csv=None)
+    test_imgs=[crop_symbol(img) for img in test_imgs]
     dump(os.path.join(DATA_DIR,TEST_DATASET_PKL), test_imgs,test_labels,test_image_ids, classes)
 
 
