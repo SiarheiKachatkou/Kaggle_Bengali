8c55feec05d788038b4a338618f01ebe13bede99
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-05 13:40:28 +0300

    auto_cmt_submission

--- dvc_make_submission.sh
+++ dvc_make_submission.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-PY=/home/sergey/anaconda3/envs/tensorflow/bin/python
+PY=/home/sergey/anaconda3/envs/pytorch/bin/python
 
-deps="-d make_submission.py -d data/models -d data/test_datset.pkl -d data/image_gen.pkl"
+deps="-d make_submission.py -d data/models2 -d data/test_datset.pkl"
 output_file_path=data/submissions/submission.csv
 dvc run $deps -f $output_file_path.dvc -O $output_file_path $PY make_submission.py
+
+git commit -a -m auto_cmt_submission
