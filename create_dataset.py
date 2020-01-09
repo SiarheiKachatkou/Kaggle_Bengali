be02f106f5adc726ee3ac9b4398ed8facc24c6f8
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-09 13:27:20 +0300

    crop_symbols_10_pixel_unconditional_crop

--- create_dataset.py
+++ create_dataset.py
@@ -43,13 +43,17 @@ def load_parquet(path_pattern, labels_csv):
     return imgs,image_ids,labels
 
 def crop_symbol(img):
-    '''
+
+    top=10
+    left=10
+    img=img[top:-top,left:-left]
+
     _,bin=cv2.threshold(img,0,255,cv2.THRESH_OTSU)
     black=np.where(bin<230)
     left,right = min(black[1]), max(black[1])
     top,bottom = min(black[0]), max(black[0])
     img=img[top:bottom,left:right]
-    '''
+
 
     img=preproc(img)
 
