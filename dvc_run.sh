b3f54f7cc9fe66e41deaf3ebb0f37891d808f51c
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-12 12:37:06 +0300

    mnist_augm_1_800

--- dvc_run.sh
+++ dvc_run.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-for augm_count in 800 900 1000 2000 4000 8000
+for augm_count in 800 900
 do
 
-git_msg=mnist_augm_$augm_count
+git_msg=mnist_augm_1_$augm_count
 ./dvc_create_dataset.sh $git_msg $augm_count
 ./dvc_train.sh $git_msg
 
