2e33d6ac0724484b45294cb59c6889039b7f900c
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-13 13:37:26 +0300

    bengali_small

--- model_pytorch.py
+++ model_pytorch.py
@@ -211,9 +211,9 @@ class Model(ModelBase, torch.nn.Module):
         #resnet 152,resnet-101,resnet-50
         block_counts_resnet_152=[3,8,36,3]
         block_counts_resnet_101=[3,4,23,3]
-        block_counts_resnet_50=[3,4,6,3]
-        block_counts_resnet_50_mnist=[3]
-        block_counts=block_counts_resnet_50_mnist
+        lock_counts_resnet_50=[3,4,6,3]
+        #block_counts_resnet_50_mnist=[3]
+        block_counts=block_counts_resnet_50
         d=4
         self._d=d
 
