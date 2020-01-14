327c56f4273d2890a28e2c33cb6b89f850e52a0f
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-14 10:11:15 +0300

    train test max_img_counts differs

--- main_create_slices_dataset.sh
+++ main_create_slices_dataset.sh
@@ -5,14 +5,18 @@ set -x
 TAG=467addd
 DST_DIR=/home/sergey/1T/DNNDebug/Data/SlicesDataset/
 CLASS_MAP_PATH='data/raw/class_map.csv'
-MAX_IMGS_COUNT=10000
+MAX_IMGS_COUNT_TRAIN=10000
+MAX_IMGS_COUNT_TEST=1000
 ACTIVATION_NAME=conv3
 #_backbone.layer1.0.conv3
 
 
 PY=/home/sergey/anaconda3/envs/pytorch/bin/python
 
-for sub_dataset in train test
+sub_datasets=(train test)
+max_sample_counts=($MAX_IMGS_COUNT_TRAIN $MAX_IMGS_COUNT_TEST)
+
+for i in 0 1
 do
-$PY main_create_slices_dataset.py --tag=$TAG --sub_dataset=$sub_dataset --dst_dir=$DST_DIR --max_imgs_count=$MAX_IMGS_COUNT --activation_name_postfix=$ACTIVATION_NAME --class_map_path=$CLASS_MAP_PATH
+$PY main_create_slices_dataset.py --tag=$TAG --sub_dataset=${sub_datasets[$i]} --dst_dir=$DST_DIR --max_imgs_count=${max_sample_counts[$i]} --activation_name_postfix=$ACTIVATION_NAME --class_map_path=$CLASS_MAP_PATH
 done
