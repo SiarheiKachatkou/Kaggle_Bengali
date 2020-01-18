736199cec44811cf30fcaa672ce741f7065d49aa
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-18 12:30:34 +0300

    block_6_8_dropout_reduced_64_64

--- model_pytorch.py
+++ model_pytorch.py
@@ -257,10 +257,10 @@ class Model(ModelBase, torch.nn.Module):
         self._blocks.append(SEResNetBottleNeckBlock(in_channels=m(256)))#block_4
         self._blocks.append(ResNetBasicBlock(in_channels=m(256)))
 
-        d2=2
+        d2=1
         self._blocks.append(ConvBnRelu(in_channels=m(256),out_channels=m(d2*512),stride=2))
         self._blocks.append(ConvBnRelu(in_channels=m(d2*512),out_channels=m(d2*1024),stride=2,kernel_size=3))
-        self._blocks.append(nn.Dropout(p=0.5))
+        self._blocks.append(nn.Dropout(p=0.05))
         self._blocks.append(ConvBnRelu(in_channels=m(d2*1024),out_channels=m(d2*2048),stride=2))
         '''
         for _ in range(block_counts[1]):
