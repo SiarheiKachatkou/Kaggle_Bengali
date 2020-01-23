6f0d7b67781f9221ad762828bcc2020e8a4e6838
siarhei.kachatkou <siarhei.kachatkou@regula.by>
2020-01-24 01:45:26 +0300

    fix_in_lr_scheduler

--- loss.py
+++ loss.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def calc_classes_weights(labels,classes_list):
-    betta=0.999999
+    betta=0.99
 
     weights=[]
     classes_count=np.shape(labels)[1]
@@ -16,8 +16,8 @@ def calc_classes_weights(labels,classes_list):
         freq=[l.count(unique_label) for unique_label in unique_labels]
 
         w=[(1-betta)/(1-betta**f) for f in freq]
-        total=sum(w)
-        w=[w_i/total for w_i in w]
+        #total=sum(w)
+        #w=[w_i/total for w_i in w]
         weights.append(w)
 
     return weights
