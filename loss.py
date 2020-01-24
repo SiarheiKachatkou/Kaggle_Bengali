cfe698b620fd2124d6e5f17834045a5726a37724
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-01-24 09:14:01 +0300

    debug_1

--- loss.py
+++ loss.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from consts import BETA
 
 
 def calc_classes_weights(labels,classes_list):
-    betta=0.999999
 
     weights=[]
     classes_count=np.shape(labels)[1]
@@ -15,9 +15,9 @@ def calc_classes_weights(labels,classes_list):
         l=list(l)
         freq=[l.count(unique_label) for unique_label in unique_labels]
 
-        w=[(1-betta)/(1-betta**f) for f in freq]
-        total=sum(w)
-        w=[w_i/total for w_i in w]
+        w=[(1-BETA)/(1-BETA**f) for f in freq]
+        #total=sum(w)
+        #w=[w_i/total for w_i in w]
         weights.append(w)
 
     return weights
@@ -27,7 +27,7 @@ class RecallScore(nn.Module):
         super().__init__()
         self.class_weights=torch.Tensor(classes_weights).to(device=torch.device('cuda'))
         self.classes_labels=list(range(len(classes_weights)))
-        self._raw_loss_fn=nn.CrossEntropyLoss(weight=self.class_weights)
+        self._raw_loss_fn=nn.CrossEntropyLoss()#(weight=self.class_weights)
 
     def forward(self,output,target,):
         loss=self._raw_loss_fn(output,target)
