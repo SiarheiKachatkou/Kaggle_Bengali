77e414f99c9de0d6c1bff7f05e6e240753478c5a
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-02-24 11:29:43 +0300

    apex_with_pytorch

--- install_apex.sh
+++ install_apex.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+pip install torch==1.3.1 torchvision==0.4.2
+
+
 git clone https://github.com/NVIDIA/apex
 cd apex
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
